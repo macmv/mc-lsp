65b3353d99af76377d99d9b50fd6e6db655c70bc
Neil Macneale V <neil.macneale.v@gmail.com>
2024-11-10 01:55:42 -0800

    Hookup the language server to the vscode extension

--- editor/vscode/src/extension.ts
+++ editor/vscode/src/extension.ts
@@ -1,13 +1,51 @@
 import * as vscode from "vscode";
+import {
+  Executable,
+  LanguageClient,
+  RevealOutputChannelOn,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "mclsp" is now active!');
 
-  const disposable = vscode.commands.registerCommand("mclsp.helloWorld", () => {
-    vscode.window.showInformationMessage("Hello World from mclsp!");
-  });
+  context.subscriptions.push(
+    vscode.commands.registerCommand("mclsp.helloWorld", () => {
+      vscode.window.showInformationMessage("Hello World from mclsp!");
+    })
+  );
 
-  context.subscriptions.push(disposable);
+  const exec: Executable = {
+    command: "/home/macmv/Desktop/programming/rust/mclsp/target/release/mc-lsp",
+    transport: TransportKind.stdio,
+  };
+
+  const serverOptions: ServerOptions = {
+    run: exec,
+    debug: exec,
+  };
+
+  const clientOptions = {
+    documentSelector: [
+      {
+        scheme: "file",
+        language: "mcmodel",
+      },
+    ],
+    outputChannel: vscode.window.createOutputChannel("MC LSP"),
+    revealOutputChannelOn: RevealOutputChannelOn.Info,
+  };
+
+  const client = new LanguageClient(
+    "mclsp",
+    "MC LSP",
+
+    serverOptions,
+    clientOptions
+  );
+
+  await client.start();
 }
 
 export function deactivate() {}
