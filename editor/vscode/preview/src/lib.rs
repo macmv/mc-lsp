5c34dc7d342be7169aa6663e4a74ca1eee435638
Neil Macneale V <neil.macneale.v@gmail.com>
2024-11-10 16:40:22 -0800

    Center the cube, and derive the model transform from fields

--- editor/vscode/preview/src/lib.rs
+++ editor/vscode/preview/src/lib.rs
@@ -1,4 +1,4 @@
-use nalgebra::Matrix4;
+use nalgebra::{point, vector, Matrix4};
 use wasm_bindgen::prelude::*;
 
 mod render;
@@ -9,6 +9,8 @@ struct Preview {
   proj:  Matrix4<f32>,
   view:  Matrix4<f32>,
   model: Matrix4<f32>,
+
+  rotation_yaw: f64,
 }
 
 #[wasm_bindgen(start)]
@@ -29,16 +31,21 @@ impl Preview {
     Preview {
       proj:  Matrix4::new_perspective(1.0, 1.0, 0.1, 100.0),
       view:  Matrix4::look_at_rh(
-        &nalgebra::Point3::new(0.0, 4.0, 5.0),
-        &nalgebra::Point3::new(0.0, 0.0, 0.0),
-        &nalgebra::Vector3::new(0.0, 1.0, 0.0),
+        &point![0.0, 4.0, 5.0],
+        &point![0.0, 0.0, 0.0],
+        &vector![0.0, 1.0, 0.0],
       ),
       model: Matrix4::identity(),
+
+      rotation_yaw: 0.0,
     }
   }
 
   fn draw(&mut self, render: &Render) {
-    self.model *= Matrix4::new_rotation(nalgebra::Vector3::new(0.0, 1.0, 0.0) * 0.01);
+    self.rotation_yaw += 0.01;
+
+    self.model = Matrix4::new_rotation(&vector![0.0, 1.0, 0.0] * self.rotation_yaw as f32)
+      * Matrix4::new_translation(&vector![-0.5, -0.5, -0.5]);
 
     render.draw(
       &self.proj.data.as_slice(),
